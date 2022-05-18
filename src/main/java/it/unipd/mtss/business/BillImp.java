2dca5b1053606b102176aa4e279e7f2c96f2c3ab
Davide <davide.baggio.01@gmail.com>
2022-05-18 18:09:06 +0200

    close #2

--- src/main/java/it/unipd/mtss/business/BillImp.java
+++ src/main/java/it/unipd/mtss/business/BillImp.java
@@ -14,6 +14,8 @@ import java.util.List;
 public class BillImp implements Bill {
     public double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException {
         double tot = 0.0;
+        int countProcessor = 0;
+        double minProcessor = Double.MAX_VALUE;
 
         if(itemsOrdered == null) {
             throw new BillException("Items ordered list cannot be null");
@@ -26,7 +28,17 @@ public class BillImp implements Bill {
         }
 
         for(EItem item : itemsOrdered) {
+            if(item.getItemType() == EItem.item.Processor){
+                countProcessor++;
+                if(item.getPrice() < minProcessor){
+                    minProcessor = item.getPrice();
+                }
+            }
             tot += item.getPrice();
+        }
+
+        if(countProcessor > 5){
+            tot -= (minProcessor/2);
         }
 
         return tot;
